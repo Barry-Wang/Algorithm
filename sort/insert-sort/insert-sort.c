d6f8c1a8e6549a0df6e9b84027a86c0dc6fff1a7
Barry-Wang <barrywang23@gmail.com>
2017-06-05 22:14:07 +0800

    it's wrong, the last sort way is named choose sort, this is the real insert sort

--- sort/insert-sort/insert-sort.c
+++ sort/insert-sort/insert-sort.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+ 找出从当前位置的下一个开始开始的最小数与当前的数做比较，如果如何要求就交换
+
+*/
+
 void insert_sort (int *a, int length) {
 
     for(int i = 0; i < length - 1; i++) {
@@ -36,10 +41,35 @@ void insert_sort (int *a, int length) {
 
 
 }
+
+/*
+把当前位置的数字与前面已经排序好的比较，如果满足条件就交换，遇到第一个不满足条件的就停止这次比较
+*/
+
+
+void insert_sort_2 (int *a, int count) {
+
+    for (int i = 0; i < count; ++i)
+    {
+        int key = a[i];
+
+        int j = i - 1;
+        while (j >= 0 && a[j] > key) {
+
+            a[j+1] = a[j];
+            j--;
+        }
+
+        a[j+1] = key;
+
+    }
+
+}
+
 int main(int argc, char const *argv[]) {
 
-    int a[] = {9};
-    insert_sort(a, sizeof(a) / sizeof(a[0]));
+    int a[] = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+    insert_sort_2(a, sizeof(a) / sizeof(a[0]));
 
     for (int i = 0; i < sizeof(a) / sizeof(a[0]); ++i)
     {
